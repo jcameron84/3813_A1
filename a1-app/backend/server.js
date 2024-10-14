c78cafbe37288846c2262a8e5eaf06bc8ea82f10
Joel <joelcameron0406@gmail.com>
2024-10-14 14:43:43 +1000

    have been forgetting to commit, changes made to server to integrate mogo and more

--- a1-app/backend/server.js
+++ a1-app/backend/server.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-res.send('Hello MEAN Stack!');
-});
+const { MongoClient } = require('mongodb');
 
-app.listen(port, () => {
-console.log(`Server is running on port ${port}`);
+const url = 'mongodb://localhost:27017';
+const dbName = 'chatApp';
+
+const client = new MongoClient(url);
+
+async function connectToDatabase() {
+    try {
+      await client.connect();
+      console.log('Connected to MongoDB');
+      const db = client.db(dbName);
+      return db;
+    } catch (err) {
+      console.error('Error connecting to MongoDB', err);
+      process.exit(1);
+    }
+}
+
+
+connectToDatabase().then((db) => {
+  app.get('/', (req, res) => {
+    res.send('Hello MEAN Stack!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
